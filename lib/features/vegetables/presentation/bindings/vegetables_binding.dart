ef9e82bb5383b4c42fb62c2b550bf778a1d3cbdc
juank <juankpaez31@gmail.com>
2026-09-02 23:19:06 -0500

    changes in bascula

--- lib/features/vegetables/presentation/bindings/vegetables_binding.dart
+++ lib/features/vegetables/presentation/bindings/vegetables_binding.dart
@@ -20,25 +20,34 @@ import '../../data/services/vegetable_order_pdf_service.dart';
 class VegetablesBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<VegetablesController>(
-      () => VegetablesController(
-        getVegetableCategoriesUseCase: getIt<GetVegetableCategoriesUseCase>(),
-        saveVegetableCategoryUseCase: getIt<SaveVegetableCategoryUseCase>(),
-        deleteVegetableCategoryUseCase: getIt<DeleteVegetableCategoryUseCase>(),
-        getVegetableItemsUseCase: getIt<GetVegetableItemsUseCase>(),
-        saveVegetableItemUseCase: getIt<SaveVegetableItemUseCase>(),
-        deleteVegetableItemUseCase: getIt<DeleteVegetableItemUseCase>(),
-        createVegetableSaleUseCase: getIt<CreateVegetableSaleUseCase>(),
-        getVegetableSalesUseCase: getIt<GetVegetableSalesUseCase>(),
-        getVegetableSaleByIdUseCase: getIt<GetVegetableSaleByIdUseCase>(),
-        createVegetableOrderUseCase: getIt<CreateVegetableOrderUseCase>(),
-        getVegetableOrdersUseCase: getIt<GetVegetableOrdersUseCase>(),
-        getVegetableOrderByIdUseCase: getIt<GetVegetableOrderByIdUseCase>(),
-        scaleService: getIt<ScaleService>(),
-        printerService: getIt<VegetablePrinterService>(),
-        orderPdfService: getIt<VegetableOrderPdfService>(),
-      ),
-      fenix: true,
-    );
+    // VegetablesController mantiene la conexión en vivo con la báscula
+    // (puerto serial). Debe sobrevivir mientras se navega entre las
+    // pantallas de verduras (venta, configuración de báscula, catálogo,
+    // etc.); si se registrara con lazyPut/fenix, GetX lo destruiría al
+    // salir de una de esas pantallas y onClose() cerraría la báscula real,
+    // dejando "báscula no conectada" en el resto de pantallas aunque el
+    // puerto siga guardado correctamente.
+    if (!Get.isRegistered<VegetablesController>()) {
+      Get.put<VegetablesController>(
+        VegetablesController(
+          getVegetableCategoriesUseCase: getIt<GetVegetableCategoriesUseCase>(),
+          saveVegetableCategoryUseCase: getIt<SaveVegetableCategoryUseCase>(),
+          deleteVegetableCategoryUseCase: getIt<DeleteVegetableCategoryUseCase>(),
+          getVegetableItemsUseCase: getIt<GetVegetableItemsUseCase>(),
+          saveVegetableItemUseCase: getIt<SaveVegetableItemUseCase>(),
+          deleteVegetableItemUseCase: getIt<DeleteVegetableItemUseCase>(),
+          createVegetableSaleUseCase: getIt<CreateVegetableSaleUseCase>(),
+          getVegetableSalesUseCase: getIt<GetVegetableSalesUseCase>(),
+          getVegetableSaleByIdUseCase: getIt<GetVegetableSaleByIdUseCase>(),
+          createVegetableOrderUseCase: getIt<CreateVegetableOrderUseCase>(),
+          getVegetableOrdersUseCase: getIt<GetVegetableOrdersUseCase>(),
+          getVegetableOrderByIdUseCase: getIt<GetVegetableOrderByIdUseCase>(),
+          scaleService: getIt<ScaleService>(),
+          printerService: getIt<VegetablePrinterService>(),
+          orderPdfService: getIt<VegetableOrderPdfService>(),
+        ),
+        permanent: true,
+      );
+    }
   }
 }
